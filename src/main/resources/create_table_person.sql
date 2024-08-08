a5f63e43e5a2297563f5767902bc4a6d172bf875
tsarkoff <tsarkov.vladimir@gmail.com>
2024-08-08 23:36:03 +0300

    ГзUpdated by expert notes (text changed with varchar()).

--- src/main/resources/create_table_person.sql
+++ src/main/resources/create_table_person.sql
@@ -11,6 +11,19 @@ create table contacts.person
     primary key (name, surname, age)
 );
 
+drop table contacts.person;
+
+create table contacts.person
+(
+    id             bigserial,
+    name           varchar(255) not null,
+    surname        varchar(255) not null,
+    age            int  not null default 18,
+    phone_number   varchar(20),
+    city_of_living varchar(255),
+    primary key (name, surname, age)
+);
+
 insert into contacts.person (name, surname, age, phone_number, city_of_living)
 values ('Ivan', 'Ivanov', 18, '+7-095', 'Moscow'),
        ('Jan', 'Chanson', 28, '+3-102', 'Helsinki'),
