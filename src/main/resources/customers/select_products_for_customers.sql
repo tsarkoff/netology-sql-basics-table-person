e20281db83450db2dd3a48791828764f74d22e70
tsarkoff <tsarkov.vladimir@gmail.com>
2024-08-15 17:43:23 +0300

    Forgotten LOWER() added for WHERE filter.

--- src/main/resources/customers/select_products_for_customers.sql
+++ src/main/resources/customers/select_products_for_customers.sql
@@ -2,4 +2,4 @@ select o.product_name, c.name, c.surname, o.date, o.amount
 from contacts.CUSTOMERS as c
          inner join contacts.ORDERS as o
                     on c.id = o.customer_id
-where c.name = 'Alexey';
+where lower(c.name) = 'alexey';
